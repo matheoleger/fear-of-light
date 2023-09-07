512370744e8da46a5c940aef4dc73e15b24772a3
matheoleger <matheo.leger@ynov.com>
2023-09-07 19:54:55 +0200

    chore(lSensor): Clean Light sources verification
    Now we have a method to Detect and Track light sources. And the state of the LightSensor (isEnabled) is changed separatly from the DetectAndTrackLightSources method

--- Scripts/Mechanism/LightSensor.cs
+++ Scripts/Mechanism/LightSensor.cs
@@ -9,7 +9,7 @@ public partial class LightSensor : Activator
 	private AnimatedSprite2D _animatedSprite;
 
 	private List<Area2D> detectedLightSources = new List<Area2D>(); 
-	private List<Area2D> lightSourcesInArea = new List<Area2D>(); 
+	private List<Area2D> areasCurrentlyInArea2D = new List<Area2D>(); 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -20,46 +20,53 @@ public partial class LightSensor : Activator
 
 	public override void _PhysicsProcess(double delta)
 	{
-		foreach(Area2D lightSource in lightSourcesInArea)
-		{
-			_raycast2D.TargetPosition = lightSource.GetParent<Node2D>().Position - _raycast2D.GetParent<Node2D>().Position;
-			_raycast2D.ForceRaycastUpdate();
-			
-			GodotObject collider = _raycast2D.GetCollider();
-			if(collider == null) continue;
-			Type colliderType = collider.GetType();
-
-			if(_raycast2D.IsColliding() && colliderType != typeof(TileMap))
-			{
-				if(!IsEnabled) IsEnabled = true;
-
-				if(!detectedLightSources.Contains(lightSource))
-					detectedLightSources.Add(lightSource);
-
-			} else if(_raycast2D.IsColliding())
-			{
-				if(detectedLightSources.Contains(lightSource))
-					detectedLightSources.Remove(lightSource);
-			}
-		}
+		DetectAndTrackLightSources();
 
 		if(detectedLightSources.Count == 0 && IsEnabled)
 			IsEnabled = false;
+		else if(detectedLightSources.Count > 0 && !IsEnabled)
+			IsEnabled = true;
 
 		HandleAnimation();
 	}
 
+	private void DetectAndTrackLightSources()
+	{
+		foreach(Area2D areaToVerified in areasCurrentlyInArea2D)
+		{
+			Node2D parentOfVerifiedArea = areaToVerified.GetParent<Node2D>();
+
+			_raycast2D.TargetPosition = parentOfVerifiedArea.Position - Position;
+			_raycast2D.ForceRaycastUpdate();
+			
+			GodotObject collider = _raycast2D.GetCollider();
+			if(collider == null) continue;
+			bool isColliderALightSource = collider is Node2D nodeCollider && nodeCollider.IsInGroup("LightSources");
+
+			if(_raycast2D.IsColliding() && isColliderALightSource)
+			{
+				if(!detectedLightSources.Contains(areaToVerified))
+					detectedLightSources.Add(areaToVerified);
+
+			} else if(_raycast2D.IsColliding())
+			{
+				if(detectedLightSources.Contains(areaToVerified))
+					detectedLightSources.Remove(areaToVerified);
+			}
+		}
+	}
+
 	public void _OnArea2dAreaEntered(Area2D area)
 	{
-		if(!lightSourcesInArea.Contains(area))
-			lightSourcesInArea.Add(area);
+		if(!areasCurrentlyInArea2D.Contains(area))
+			areasCurrentlyInArea2D.Add(area);
 	}
 
 	public void _OnArea2dAreaExited(Area2D area)
 	{
 
-		if(lightSourcesInArea.Contains(area))
-			lightSourcesInArea.Remove(area);
+		if(areasCurrentlyInArea2D.Contains(area))
+			areasCurrentlyInArea2D.Remove(area);
 
 		if(detectedLightSources.Contains(area))
 			detectedLightSources.Remove(area);		
