456e1115012d0c6fb203b77e0e21c7474795648b
Mathéo LEGER <71370253+matheoleger@users.noreply.github.com>
2024-01-28 19:05:59 +0100

    Feat(LightCrystal): Add animations for apparition and disappearance (#12)
    * feat(LightCrystal): Add animations
    
    Now we have animation for apparition and disparition
    
    * feat(lightCrystal): Remove at the right frame
    
    Now we wait for firsts or lasts frame of the default animation (because of animation frame match)
    
    * fix(lightCrystal): English translation
    
    * fix(lightCrystal): English translation

--- Scripts/LightCrystal.cs
+++ Scripts/LightCrystal.cs
@@ -5,17 +5,46 @@ public partial class LightCrystal : Node2D
 {
 
 	private AnimatedSprite2D _animatedSprite;
-	
+
+	private bool isDestroying = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        _animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		_animatedSprite.Play("default");
+		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_animatedSprite.Play("apparition");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		HandleDestroying();
 	}
+
+	public void Destroy()
+	{
+		isDestroying = true;
+	}
+
+	private void HandleDestroying()
+	{
+		if (!isDestroying) return;
+
+		bool isRightFrame = _animatedSprite.Frame == 13 
+		|| _animatedSprite.Frame == 12 
+		|| _animatedSprite.Frame == 0 
+		|| _animatedSprite.Frame == 1;
+
+		if (isRightFrame)
+			_animatedSprite.Play("disappearance");
+	}
+
+	public void _OnAnimatedSprite2dAnimationFinished()
+	{
+		if (_animatedSprite.Animation == "apparition")
+			_animatedSprite.Play("default");
+
+		if (_animatedSprite.Animation == "disappearance")
+			GetTree().CurrentScene.RemoveChild(this);
+	}
+
 }
