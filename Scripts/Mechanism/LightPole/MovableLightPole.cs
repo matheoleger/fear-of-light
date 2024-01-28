15fd9be2720acd8764735cac54bcf6a9a0bdca61
matheoleger <matheo.leger@ynov.com>
2024-01-28 17:03:43 +0100

    refacto(lightPole): Move methods to the helper
    I use RechargeableLightPoleHelper for FadeIn and FadeOut light. Also I use it for InitializeRechargeableLightPole.

--- Scripts/Mechanism/LightPole/MovableLightPole.cs
+++ Scripts/Mechanism/LightPole/MovableLightPole.cs
@@ -10,7 +10,7 @@ public partial class MovableLightPole : RigidBody2D, ILightPole
 
 	public AnimatedSprite2D _AnimatedSprite { get; set; }
 
-	public PointLight2D _PointLight2D {get; set;}
+	public PointLight2D _PointLight2D { get; set; }
 
 	public Area2D _LightArea2D { get; set; }
 
@@ -18,8 +18,8 @@ public partial class MovableLightPole : RigidBody2D, ILightPole
 
 	public Tween RechargeableTween { get; set; }
 
-	private readonly List<Area2D> detectedLightSources = new List<Area2D>(); 
-	private readonly List<Area2D> areasCurrentlyInArea2D = new List<Area2D>(); 
+	private readonly List<Area2D> detectedLightSources = new List<Area2D>();
+	private readonly List<Area2D> areasCurrentlyInArea2D = new List<Area2D>();
 
 
 	// Called when the node enters the scene tree for the first time.
@@ -31,103 +31,69 @@ public partial class MovableLightPole : RigidBody2D, ILightPole
 		_RayCast2D = GetNode<RayCast2D>("RayCast2D");
 
 		// Initialize the rechargeableLightPole
-		if(IsRechargeable)
-			InitializeRechargeableLightPole();
-
-		RechargeableLightPoleHelper.ChangeAnimation(this);
+		RechargeableLightPoleHelper.InitializeRechargeableLightPole(this);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		if(!IsRechargeable) return;
+		if (!IsRechargeable) return;
 
 		DetectAndTrackLightSources();
 
-		if(detectedLightSources.Count >= 1)
-			FadeInLight();
+		if (detectedLightSources.Count >= 1)
+			RechargeableLightPoleHelper.FadeInLight(this);
 		else
-			FadeOutLight();
-	}
-
-	private void InitializeRechargeableLightPole()
-	{
-		_PointLight2D.Energy = 0.0f;
-
-		if(_LightArea2D.GetNode<CollisionShape2D>("CollisionShape2D").Shape is CircleShape2D circleShape2D)
-			circleShape2D.Radius = 0.0f;
-	}
-
-
-	private void FadeOutLight()
-	{
-		RechargeableTween?.Kill();
-		RechargeableTween = CreateTween();
-
-		RechargeableTween.Parallel().TweenProperty(_PointLight2D, "energy", 0.0f, 15.0f);
-
-		if(_LightArea2D.GetNode<CollisionShape2D>("CollisionShape2D").Shape is CircleShape2D circleShape2D)
-			RechargeableTween.Parallel().TweenProperty(circleShape2D, "radius", 0.0f, 15.0f);
-	}
-
-	private void FadeInLight()
-	{
-		RechargeableTween?.Kill();
-		RechargeableTween = CreateTween();
-
-		RechargeableTween.Parallel().TweenProperty(_PointLight2D, "energy", 0.6f, 4.0f);
-
-		if(_LightArea2D.GetNode<CollisionShape2D>("CollisionShape2D").Shape is CircleShape2D circleShape2D)
-			RechargeableTween.Parallel().TweenProperty(circleShape2D, "radius", 100.0f, 4.0f);
+			RechargeableLightPoleHelper.FadeOutLight(this);
 	}
 
 	private void DetectAndTrackLightSources()
 	{
-		foreach(Area2D areaToVerified in areasCurrentlyInArea2D)
+		foreach (Area2D areaToVerified in areasCurrentlyInArea2D)
 		{
 			Node2D parentOfVerifiedArea = areaToVerified.GetParent<Node2D>();
 
 			_RayCast2D.TargetPosition = parentOfVerifiedArea.Position - Position;
 			_RayCast2D.ForceRaycastUpdate();
-			
+
 			GodotObject collider = _RayCast2D.GetCollider();
-			if(collider == null) continue;
-			
+			if (collider == null) continue;
+
 			bool isColliderALightSource = collider is Node2D nodeCollider && nodeCollider.IsInGroup("LightSources");
 
-			if(_RayCast2D.IsColliding() && isColliderALightSource)
+			if (_RayCast2D.IsColliding() && isColliderALightSource)
 			{
-				if(!detectedLightSources.Contains(areaToVerified))
+				if (!detectedLightSources.Contains(areaToVerified))
 					detectedLightSources.Add(areaToVerified);
 
-			} else if(_RayCast2D.IsColliding())
+			}
+			else if (_RayCast2D.IsColliding())
 			{
-				if(detectedLightSources.Contains(areaToVerified))
+				if (detectedLightSources.Contains(areaToVerified))
 					detectedLightSources.Remove(areaToVerified);
 			}
 		}
 	}
 
-
 	public void _OnRechargeableArea2dAreaEntered(Area2D area)
 	{
-		if(!IsRechargeable) return;
+		if (!IsRechargeable) return;
 
 
-		if(!areasCurrentlyInArea2D.Contains(area) && area.GetParent() != this)
+		if (!areasCurrentlyInArea2D.Contains(area) && area.GetParent() != this)
 			areasCurrentlyInArea2D.Add(area);
 
 	}
 
 	public void _OnRechargeableArea2dAreaExited(Area2D area)
 	{
-		if(!IsRechargeable) return;
+		if (!IsRechargeable) return;
 
-		if(areasCurrentlyInArea2D.Contains(area))
+		if (areasCurrentlyInArea2D.Contains(area))
 			areasCurrentlyInArea2D.Remove(area);
-	
 
-		if(detectedLightSources.Contains(area))
-			detectedLightSources.Remove(area);	
+
+		if (detectedLightSources.Contains(area))
+			detectedLightSources.Remove(area);
 	}
 }
--- Scripts/Mechanism/LightPole/RechargeableLightPoleHelper.cs
+++ Scripts/Mechanism/LightPole/RechargeableLightPoleHelper.cs
@@ -3,43 +3,80 @@ using System;
 
 public partial class RechargeableLightPoleHelper : Node
 {
-    /// <remarks>
+	/// <remarks>
 	///     <para>
-	///         This method can be called in the <c>_Ready</c> method of LightPole classes to define the animation (according to <c>IsRechargeable</c>)
+	///         This method can be called in the <c>_Ready</c> method of LightPole classes to initialize values for RechargeableLightPole (according to <c>IsRechargeable</c>)
 	///     </para>
 	///     <para>
-	///         Define the animation of the lightPole.
+	///         Define default values of the lightPole (according to <c>IsRechargeable</c>).
 	///     </para>
 	/// </remarks>
-    public static void ChangeAnimation(ILightPole lightPole)
+	public static void InitializeRechargeableLightPole(ILightPole lightPole)
 	{
-		if(lightPole.IsRechargeable)
+		ChangeAnimation(lightPole);
+
+		if (!lightPole.IsRechargeable) return;
+
+		lightPole._PointLight2D.Energy = 0.0f;
+
+		if (lightPole._LightArea2D.GetNode<CollisionShape2D>("CollisionShape2D").Shape is CircleShape2D circleShape2D)
+			circleShape2D.Radius = 0.0f;
+
+	}
+
+	private static void ChangeAnimation(ILightPole lightPole)
+	{
+		if (lightPole.IsRechargeable)
 			lightPole._AnimatedSprite.Play("rechargeable");
-		else 
+		else
 			lightPole._AnimatedSprite.Play("default");
 	}
 
-	public static void FadeOutLight(ILightPole lightpole)
+	public static void FadeOutLight(LightPole lightPole)
 	{
-		// SceneTree tree = lightpole._PointLight2D.GetParent().GetTree();
-		// // SceneTree pointLightTree = lightpole._PointLight2D.GetTree();
-		
-		// Tween tween = tree.CreateTween();
+		lightPole.RechargeableTween?.Kill();
+		lightPole.RechargeableTween = lightPole.CreateTween();
 
-		// tween.
+		HandleFadeOutLight(lightPole);
+	}
 
-		lightpole.RechargeableTween ??= lightpole._PointLight2D.GetParent().GetTree().CreateTween();
+	public static void FadeOutLight(MovableLightPole movableLightPole)
+	{
+		movableLightPole.RechargeableTween?.Kill();
+		movableLightPole.RechargeableTween = movableLightPole.CreateTween();
 
+		HandleFadeOutLight(movableLightPole);
+	}
 
-		lightpole.RechargeableTween.Parallel().TweenProperty(lightpole._PointLight2D, "energy", 0.0f, 15.0f);
-		
-		// CircleShape2D lightAreaCircleShape = lightpole._LightArea2D.GetNode<CollisionShape2D>("CollisionShape2D").Shape.GetType()
+	public static void FadeInLight(LightPole lightPole)
+	{
+		lightPole.RechargeableTween?.Kill();
+		lightPole.RechargeableTween = lightPole.CreateTween();
 
-		
+		HandleFadeOutLight(lightPole);
+	}
 
-		// GD.Print(lightpole._LightArea2D.GetNode<CollisionShape2D>("CollisionShape2D").Shape.GetType());
+	public static void FadeInLight(MovableLightPole movableLightPole)
+	{
+		movableLightPole.RechargeableTween?.Kill();
+		movableLightPole.RechargeableTween = movableLightPole.CreateTween();
 
-		if(lightpole._LightArea2D.GetNode<CollisionShape2D>("CollisionShape2D").Shape is CircleShape2D circleShape2D)
-			lightpole.RechargeableTween.Parallel().TweenProperty(circleShape2D, "radius", 0.0f, 15.0f);
+		HandleFadeInLight(movableLightPole);
+	}
+
+	private static void HandleFadeOutLight(ILightPole lightPole)
+	{
+		lightPole.RechargeableTween.Parallel().TweenProperty(lightPole._PointLight2D, "energy", 0.0f, 15.0f);
+
+		if (lightPole._LightArea2D.GetNode<CollisionShape2D>("CollisionShape2D").Shape is CircleShape2D circleShape2D)
+			lightPole.RechargeableTween.Parallel().TweenProperty(circleShape2D, "radius", 0.0f, 15.0f);
+	}
+
+	private static void HandleFadeInLight(ILightPole lightPole)
+	{
+		lightPole.RechargeableTween.Parallel().TweenProperty(lightPole._PointLight2D, "energy", 0.6f, 4.0f);
+
+		if (lightPole._LightArea2D.GetNode<CollisionShape2D>("CollisionShape2D").Shape is CircleShape2D circleShape2D)
+			lightPole.RechargeableTween.Parallel().TweenProperty(circleShape2D, "radius", 100.0f, 4.0f);
 	}
 }
