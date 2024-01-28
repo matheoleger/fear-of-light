bb9f63cc0dff275fa78c03ae74a21373e641f059
matheoleger <matheo.leger@ynov.com>
2024-01-28 17:45:26 +0100

    feat(ILightPole): Add properties in ILightPole

--- Scripts/Mechanism/LightPole/ILightPole.cs
+++ Scripts/Mechanism/LightPole/ILightPole.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public interface ILightPole
 {
@@ -23,11 +24,44 @@ public interface ILightPole
 	/// </remarks>
 	abstract AnimatedSprite2D _AnimatedSprite { get; set; }
 
-
+	/// <remarks>
+	///     <para>
+	///         This <c>PointLight2D</c> must be define in the <c>_Ready</c> method
+	///     </para>
+	///     <para>
+	///         PointLight2D of the LightPole (light source).
+	///     </para>
+	/// </remarks>
 	abstract PointLight2D _PointLight2D { get; set; }
 
+	/// <remarks>
+	///     <para>
+	///         This <c>Area2D</c> must be define in the <c>_Ready</c> method
+	///     </para>
+	///     <para>
+	///         Area2D to detect a light source (to recharge the lightPole).
+	///     </para>
+	/// </remarks>
 	abstract Area2D _LightArea2D { get; set; }
 
-	abstract Tween RechargeableTween {get; set; }
+	/// <remarks>
+	///     <para>
+	///         This <c>RayCast2D</c> must be define in the <c>_Ready</c> method
+	///     </para>
+	///     <para>
+	///         RayCast2D to detect a light source (and verify if it's through an object/wall) (to recharge the lightPole).
+	///     </para>
+	/// </remarks>
+	abstract RayCast2D _RayCast2D { get; set; }
 
+
+	/// <remarks>
+	///     <para>
+	///         This <c>Tween</c> must be define in the <c>_Ready</c> method
+	///     </para>
+	///     <para>
+	///         Tween to fade in and out the light (used in <see cref="RechargeableLightPoleHelper"/>).
+	///     </para>
+	/// </remarks>
+	abstract Tween RechargeableTween {get; set; }
 }
